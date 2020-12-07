c7268292f3c81b9e3c7321ce70f0720c4e56c553
Juan Macias <juan.maciascoronado@pepsico.com>
2020-12-07 17:22:58 -0600

    Add Unit Test Examples

--- core/test_ch20UnitTest.py
+++ core/test_ch20UnitTest.py
@@ -35,3 +35,50 @@ def test_assert_exp():
     # Test Exceptions
     with pytest.raises(ValueError):
         divide(25, 0)
+
+
+# follow : https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
+# How to mock?
+
+
+# Fixtures
+
+@pytest.fixture
+def empty_array():
+    '''Empty Arrya'''
+    return []
+
+
+@pytest.fixture
+def numbers():
+    return [1, 2, 3]
+
+
+def test_sum_all(numbers):  # You pass the named fixture
+    assert sum(numbers) == 6
+
+
+def test_sum_empty(empty_array):  # You pass the named fixture
+    assert sum(empty_array) == 0
+
+
+# List Fixtures using pytest --fixtures
+
+# %%
+@pytest.mark.parametrize("param1,param2", [
+    (1, 1),
+    (2, 2),
+])
+def test_sum_param(empty_array, param1, param2):
+    empty_array = []
+    empty_array.append(param1)
+    empty_array.append(param2)
+    assert sum(empty_array) == param1 + param2
+
+# TODO: Mocking
+
+
+'''                                                                       
+test_ch20UnitTest.py::test_sum_param[1-1] PASSED
+test_ch20UnitTest.py::test_sum_param[2-2] PASSED
+'''
