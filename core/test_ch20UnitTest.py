40dd145a5e409810a20767d24d307fc989f2bebd
Juan Macias <juan.maciascoronado@pepsico.com>
2020-12-07 11:31:59 -0600

    Add test examples

--- core/test_ch20UnitTest.py
+++ core/test_ch20UnitTest.py
@@ -3,13 +3,18 @@
 # to execute a chunk of code just use `# %%` And
 # Ctrl+ Enter will execute the code
 
-# Already in Pipfile
+# Pytest Already in Pipfile
+# Course uses unittest
 
 # %% TEST
 from typing import Union
+import pytest
 
 
 def divide(dividend: Union[int, float], divisor: Union[int, float]):
+    if divisor == 0:
+        raise ValueError("The divisor cannot be zero")
+
     return dividend / divisor
 
 # %%
@@ -19,3 +24,14 @@ def divide(dividend: Union[int, float], divisor: Union[int, float]):
 
 def test_divide():  # Configure VS CODE to find test methods
     assert divide(1, 1) == 1.0
+
+
+def test_divide_negative():
+    # Test Approx values
+    assert pytest.approx(-5.0) == divide(15, -3)
+
+
+def test_assert_exp():
+    # Test Exceptions
+    with pytest.raises(ValueError):
+        divide(25, 0)
