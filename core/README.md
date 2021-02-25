37b041a813d212f9e5f1bcaced7de44984720987
Juan Macias <juan.maciascoronado@pepsico.com>
2021-02-25 17:01:52 -0600

    Continue chapter 23 - remove devcontainer

---
+++ core/README.md
@@ -0,0 +1,10 @@
+# Examples from The Complete Python Course
+
+Exercises from the course [The Complete Python Course](https://learning.oreilly.com/videos/the-complete-python/9781839217289/)
+
+## Getting Started
+
+```
+   pipenv install --dev --system
+```
+
--- core/ch23.py
+++ core/ch23.py
@@ -8,7 +8,7 @@
 
 # %% Variable
 
-from ch13Async import greet
+# from ch13Async import greet
 
 
 x = 15  # Python Evaluates the value expresion first, in this case 15, and left later
@@ -23,7 +23,7 @@ result = price * (1 - discount)
 print(result)
 
 name = "test"
-name = 'bob'
+name = "bob"
 print(name)
 print(name * 2)  # Multi has meaning for strings this double the string
 
@@ -40,18 +40,18 @@ print(b)
 
 # Since 3.6
 
-name = 'Bob'
-greeting = 'Hello, Bob'
+name = "Bob"
+greeting = "Hello, Bob"
 print(greeting)
 # Embed var inside Strings
-greeting = f'Hello, {name}'  # When evaluated , greeting is not dinamic
+greeting = f"Hello, {name}"  # When evaluated , greeting is not dinamic
 print(greeting)
-name = 'Rolf'
+name = "Rolf"
 print(greeting)
-greeting = 'Hello, {name}'  # Make it a template
+greeting = "Hello, {name}"  # Make it a template
 print(name)
 print(greeting.format(name=name))  # Pass the current value using kargs
-greeting = 'Hello, {}'  # Make it a template
+greeting = "Hello, {}"  # Make it a template
 print(greeting.format(name))  # Pass the current value using position
 
 
@@ -60,39 +60,39 @@ input_str = input("Input:")
 input_int = int(input_str)
 input_cal = input_int / 10.8
 # Inside FStr you cna format numbers using `:.PresicionNumf `
-print(f'{input_cal:.2f}')
+print(f"{input_cal:.2f}")
 
 # %% List Tuples and set
 
-l = ['Bob', 'Rolf', 'Ann']  # Mantain the order
-t = ('Bob', 'Rolf', 'Ann')  # You cannot modify the tuple
-s = {'Bob', 'Rolf', 'Ann'}
+l = ["Bob", "Rolf", "Ann"]  # Mantain the order
+t = ("Bob", "Rolf", "Ann")  # You cannot modify the tuple
+s = {"Bob", "Rolf", "Ann"}
 
 print(t[2])
 
-l[0] = 'Juan'
+l[0] = "Juan"
 print(l)
 
-'''  Error Type error
+"""  Error Type error
 t[0] = 'Juan'
 print(t)
-'''
-l.append('test')
-s.add('Juan')
+"""
+l.append("test")
+s.add("Juan")
 print(l)
 print(s)
 
 # %% Advance Set Ops
 
-friends = {'Bob', 'Rolf', 'Ann'}
-abroad = {'Bob', 'Ann'}
+friends = {"Bob", "Rolf", "Ann"}
+abroad = {"Bob", "Ann"}
 local = friends.difference(abroad)
 print(local)
 
 all_f = local.union(abroad)
 print(all_f)
 
-friends_2 = {'Bob', 'Rolf', 'Ann', 'Juan'}
+friends_2 = {"Bob", "Rolf", "Ann", "Juan"}
 
 inter = friends.intersection(friends_2)
 print(inter)
@@ -128,23 +128,19 @@ print(bool(0.2))
 # %% IN Keyword
 l = [1, 2, 3]
 s = "test"
-m = {
-    'a': 1,
-    'b': 2,
-    2: '2'
-}
+m = {"a": 1, "b": 2, 2: "2"}
 print(1 in l)
 print(1.0 in l)
-print('t' in s)
-print('t' in m)
-print('a' in m)
+print("t" in s)
+print("t" in m)
+print("a" in m)
 print(1 in m)
 print(2 in m)
 
 # %% Loops
-l = ''
+l = ""
 while not l:  # Loop while empty string
-    l = input('Stop')
+    l = input("Stop")
 
 do_while_less_than_10 = 0
 
@@ -184,7 +180,7 @@ f_age = {"Joe": 29}
 print(f_age["Joe"])
 
 try:
-    f_age['Foo']
+    f_age["Foo"]
 except KeyError as e:
     print("Key Error" + str(e))
 
@@ -203,7 +199,7 @@ for i in f_age:  # By default goes to key
     print(f_age[i])
 
 # Modify
-f_age['Joe'] = 50
+f_age["Joe"] = 50
 
 print(f_age["Joe"])
 
@@ -290,4 +286,180 @@ parameters(arg1="1")
 default_arg[1] = 100  # ups you change the ob, the ref
 parameters(arg1="1")  # you get 100 for the second :S
 
-# Lambda Fns or anonymous functions
+# %%
+def padd(a, b):
+    print(a + b)
+
+
+result = padd(1, 2)
+
+print("no return statement then it returns None")
+print(result)
+
+# %%
+"""
+Lambda Fns or anonymous functions
+"""
+# No name///
+lambda x, y: x + y
+# lets assign it to a var
+add = lambda x, y: x + y
+
+print(add(1, 23))
+doubled = map(lambda x: x * x, [1, 2, 3])
+print(list(doubled))
+
+# %%
+"""
+Dictionary Comprehension
+"""
+users = {
+    "first": "last",
+    "first2": "last2",
+}
+
+user_cap = [u for u in users]  # By default it iterates the KEYS
+user_cap
+"""
+Use items to get seq of k->v and de structure to pull them appart, and 
+generate a new map, transforming k and v
+"""
+user_cap = {f.capitalize(): l.capitalize() for (f, l) in users.items()}
+user_cap
+
+# %%
+
+"""
+Unpacking Args
+"""
+
+
+def myf(*args):
+    print(args)  # Get a tuple inoder of the args
+
+
+myf(1, "q", [1, 2, 3])
+
+myf(*(1, 2, 3))  # a tuple or list to positional args
+
+myf(*[1, 2, 3])  # a tuple or list to positional args
+
+
+def myf2(a, b, *args):
+    print(a, b)
+    print(args)  # Get a tuple inoder of the args
+
+
+print("=====")
+myf2(*(1, 2, 3))  # a tuple or list to positional args
+
+myf2("a", *(1, 2, 3))  # a tuple or list to positional args
+
+myf2("a", "b", *(1, 2, 3))  # a tuple or list to positional args
+
+
+def myf3(a, b=None, *args, **kwargs):
+    print(a, b)
+    print(args)  # Get a tuple in oder of the args
+    print(kwargs)  # Get a Map of the args
+
+
+print("====")
+# myf3() ERROR position are check at evaluation
+# myf3("1", "2", 1,2,3, a=1) ERROR a is duplicated:S
+myf3("1", "2", 1, 2, 3, c=1)
+
+
+print("====")
+myf3("1", "2")  # Only positional args are checked the rest are empty
+print("====")
+myf3(a=1)  # Since b has default value we can skep
+myf3(a=1, b=3)
+print("==== UNPACKING Keyword args ")
+myf3(**dict(a=1, b=3, c=3))  # SInce a and b are provided we can pass the map
+# and is called as named params
+
+
+def myf5(*args, enforce_named_par_at_end):
+    print(args)  # Get a tuple in oder of the args
+    print(enforce_named_par_at_end)
+
+
+try:
+    myf5(1, 2, 4, 3)
+except:
+    print("Missing named arg at the end")
+
+
+myf5(1, 2, 4, 3, enforce_named_par_at_end=2)
+
+#%% OO Prog
+"""
+OO Prog
+"""
+
+
+class Student:
+    # Class value
+    TYPES = ("a", "b")
+
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+
+    # Class method - Good for factory
+    @classmethod
+    def class_m(cls):
+        print(cls)
+        return cls
+
+    @staticmethod  # a FN inside a class
+    def scm():
+        print("NO PARMAS")
+
+    # Instance Method, you get self the instance to access data
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades)
+
+    # STR is preferred over repr on print
+    # THe str method depends on this Interface
+    def __str__(self) -> str:
+        return "<MY STUDENT " + self.name + " >"
+
+    def __repr__(self) -> str:
+        return "<MY STUDENT REPR " + self.name + " >"
+
+
+print(Student("name", [10, 10]).average_grade())
+
+"""
+dunder methods on OBJS
+
+"""
+print(Student("name", [10, 10]))
+
+print(Student.class_m())
+print(Student.scm())
+
+
+#%%
+# Inheritance
+# on init call super().__init__() ....
+# If you want to call parent method do it using super()
+
+"""
+TYPE INTING  > 3.5
+"""
+from typing import List
+
+
+def my_av(se: List[int]) -> float:
+    return sum(se) / len(se)
+
+
+# Enable mypy to see the error
+
+my_av([1, 2])
+
+
+## __name___ change w/ the file you are in
