b7c16a0a9851454e2d358cf55542791ece21aa06
Juan Macias <juan.maciascoronado@pepsico.com>
2022-10-13 16:41:06 -0500

    FIX:  Returning a view versus a copy

--- py-data-analysis/py_data_analysis/ch6_data_cleaning_text.py
+++ py-data-analysis/py_data_analysis/ch6_data_cleaning_text.py
@@ -31,14 +31,23 @@ taxi.loc[209, 'PU_Address']
 #%%
 taxi.loc[203, 'PU_Address']
 #%% Some address contains airport to pickup
+
+# this access gives a warning when trying to use to update selected cols
 taxi_airport = taxi[taxi['PU_Address'].str.contains('airport')]
+# To fix use copy to create a Series rather than a VIEW
+taxi_airport = taxi[taxi['PU_Address'].str.contains('airport')].copy()
+#  or use the preferred access method loc to create a new Series
+taxi_airport = taxi.loc[taxi['PU_Address'].str.contains('airport')]
+# See more https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+
 #%%
 taxi_airport['PU_Address'].value_counts()
 #%%
 taxi_airport['PU_Address'].str[:5]
 # [-3:]
 #  This one does not work
-taxi_airport['PU_Address']=taxi_airport.loc['PU_Address'].str.replace(' ny;', 'new york;')
+taxi_airport['PU_Address']=taxi_airport['PU_Address'].str.replace(' ny;', 'new york;')
+taxi_airport['PU_Address']=taxi_airport['PU_Address'].str.replace(' nynew york;', 'new york;')
 
 taxi_airport['PU_Address'].str[:20]
 # taxi_airport['PU_Address']
@@ -49,4 +58,3 @@ taxi.loc[taxi['PU_Address'].str.startswith('best western')]
 taxi.loc[taxi['PU_Address'].str.endswith('nj;')]['PU_Address']
 # OR
 taxi[taxi['PU_Address'].str.endswith('nj;')]['PU_Address']
-
